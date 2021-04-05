799c10f86489bced0d526330d71b0160001260e9
diashismelf <58419474+diashismelf@users.noreply.github.com>
2021-04-05 17:03:47 +0300

    mi_card

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,108 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/photo.jpg'),
+                backgroundColor: Colors.blueAccent,
+              ),
+              Text(
+                  'Eegor Balint',
+                style: TextStyle(
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
+                  //fontFamily:
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'SourceSansPro',
+                  letterSpacing: 2.0,
+                  //fontFamily:
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.teal.shade900,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: ListTile (
+                    leading: Icon(
+                      Icons.phone,
+                      size: 25.0,
+                      color: Colors.teal.shade100,
+                    ),
+                    title: Text(
+                      '+7 778 099 1371',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.teal.shade900,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: ListTile (
+                    leading: Icon(
+                      Icons.email,
+                      size: 25.0,
+                      color: Colors.teal.shade100,
+                    ),
+                      title: Text(
+                      'info@eegorbalint.ru',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'SourceSansPro',
+                        letterSpacing: 2.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
